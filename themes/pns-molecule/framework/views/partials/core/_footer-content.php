49fb29c1942100f3639a25bb99eced8ced6034c0
David Beeler <david.beeler@gmail.com>
2018-03-22 10:58:11 -0400

    Add LocalBusiness schema microdata to footer

--- themes/pns-molecule/framework/views/partials/core/_footer-content.php
+++ themes/pns-molecule/framework/views/partials/core/_footer-content.php
@@ -2,42 +2,48 @@
 // =============================================================================
 // FRAMEWORK/VIEWS/CORE/_FOOTER-CONTENT.PHP
 // -----------------------------------------------------------------------------
-// Contians the content for the footer
+// Contains the content for the footer
 // no need for footer tags -those are handled in their own partials
 // =============================================================================
 ?>
 
 <div class="o-col-md-4">
 	
-	<div class="o-footer__well">
+	<div itemscope itemtype="http://schema.org/LocalBusiness" class="o-footer__well">
 
 		<h4 class="e-h4 o-footer__well__title u-color--white">
 			<?php esc_html_e( 'Let\'s Connect' ); ?>	
 		</h4>
 
-		<p class="e-p--large c-text-icon c-text-icon--phone">
+		<img itemprop="image" class="u-vis-hide c-nav__logo__img c-nav__logo__img--desktop" src="<?php echo Molecule_Router::get_img_meta( 'acf_options', 'url', 'nav_logo_desktop'); ?>" alt="<?php echo Molecule_Router::get_img_meta( 'acf_options', 'alt', 'nav_logo_desktop'); ?>">
 
-			<a class="c-menu__link u-color--blue-l" href="https://www.google.com/maps/place/Pets+In+Stitches/@39.6306803,-84.2302138,17z/data=!3m1!4b1!4m5!3m4!1s0x884089007e771bbd:0x70b4d22ce61cd225!8m2!3d39.6306803!4d-84.2280251" target="_blank">
-				8265 Springboro Pike
+		<p class="e-p--large c-text-icon c-text-icon--phone">
+			<p class="e-p--large u-color--blue-l">
+				<span itemprop="name">Pets in Stitches</span>
+			</p>
+			<a itemprop="address" itemscope itemtype="http://schema.org/PostalAddress" class="c-menu__link u-color--blue-l" href="https://www.google.com/maps/place/Pets+In+Stitches/@39.6306803,-84.2302138,17z/data=!3m1!4b1!4m5!3m4!1s0x884089007e771bbd:0x70b4d22ce61cd225!8m2!3d39.6306803!4d-84.2280251" target="_blank">
+				<span itemprop="streetAddress">8265 Springboro Pike</span>
 				<br/>
-				Miamisburg, Ohio 45343
+				<span itemprop="addressLocality">Miamisburg</span>,
+				<span itemprop="addressRegion">Ohio</span>
+				<span itemprop="postalCode">45343</span>
 			</a>
 		</p>
 
 		<p class="e-p--large c-text-icon">
 
 			<?php echo file_get_contents( molecule()->get_setting( 'svg_dir' ) . 'icon-phone.svg' ); ?>
-			<a class="c-menu__link u-color--blue-l" href="tel:937-630-3320">937-630-3320</a>
+			<a class="c-menu__link u-color--blue-l" href="tel:937-630-3320"><span itemprop="telephone">937-630-3320</span></a>
 			<br/>
 			<?php echo file_get_contents( molecule()->get_setting( 'svg_dir' ) . 'icon-printer.svg' ); ?>
-			<a class="c-menu__link u-color--blue-l" href="tel:937-630-3325">937-630-3325</a>
+			<a class="c-menu__link u-color--blue-l" href="tel:937-630-3325"><span itemprop="faxNumber">937-630-3325</span></a>
 		</p>
         
         <p>
 
-			<a class="c-icon--social" href="https://www.facebook.com/PetsInStitches/" target="_blank"><?php echo file_get_contents( molecule()->get_setting( 'svg_dir' ) . 'icon-facebook.svg' ); ?></a>
-            <a class="c-icon--social" href="https://www.youtube.com/user/PetsInStitchesClinic" target="_blank"><?php echo file_get_contents( molecule()->get_setting( 'svg_dir' ) . 'icon-youtube.svg' ); ?></a>
-            <a class="c-icon--social" href="https://www.pinterest.com/petsinstitches/" target="_blank"><?php echo file_get_contents( molecule()->get_setting( 'svg_dir' ) . 'icon-pinterest.svg' ); ?></a>
+			<a itemprop="sameAs" class="c-icon--social" href="https://www.facebook.com/PetsInStitches/" target="_blank"><?php echo file_get_contents( molecule()->get_setting( 'svg_dir' ) . 'icon-facebook.svg' ); ?></a>
+            <a itemprop="sameAs" class="c-icon--social" href="https://www.youtube.com/user/PetsInStitchesClinic" target="_blank"><?php echo file_get_contents( molecule()->get_setting( 'svg_dir' ) . 'icon-youtube.svg' ); ?></a>
+            <a itemprop="sameAs" class="c-icon--social" href="https://www.pinterest.com/petsinstitches/" target="_blank"><?php echo file_get_contents( molecule()->get_setting( 'svg_dir' ) . 'icon-pinterest.svg' ); ?></a>
 
 		</p>
 
