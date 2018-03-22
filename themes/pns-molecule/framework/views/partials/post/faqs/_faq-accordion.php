d1fa14623438422efaff3c16e97355fcf5621210
David Beeler <david.beeler@gmail.com>
2018-03-22 19:09:45 -0400

    Correct code style

--- themes/pns-molecule/framework/views/partials/post/faqs/_faq-accordion.php
+++ themes/pns-molecule/framework/views/partials/post/faqs/_faq-accordion.php
@@ -15,10 +15,11 @@ $answer = wp_kses_post( get_field( 'faq_answer', $output['faq_id'] ) );
 		<h5 itemprop="name" class="e-h6 c-accordion--simple__header__title u-text-up"><?php echo $output['faq_title']; ?></h5>
 
 		<div class="c-accordion__icon c-accordion__icon--simple">
-			<div class="c-accordion__icon-bar 
-			c-accordion__icon-bar--1"></div>
+
+			<div class="c-accordion__icon-bar c-accordion__icon-bar--1"></div>
 
 			<div class="c-accordion__icon-bar c-accordion__icon-bar--2"></div>
+
 		</div>
 
 	</div>
@@ -26,7 +27,9 @@ $answer = wp_kses_post( get_field( 'faq_answer', $output['faq_id'] ) );
 	<div class="c-accordion--simple__body u-clearfix">
 
 		<div itemprop="acceptedAnswer" itemscope itemtype="http://schema.org/Answer" class="e-p--common c-accordion--simple__content">
+
 			<span itemprop="text"<?php echo $answer; ?></span>
+			
 		</div>
 
 	</div>
