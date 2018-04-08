0c73ea3f303b247e8d7609a078a0f342b297b429
codingwithchris <chris@codewolfseven.com>
2018-04-08 10:13:10 -0400

    Build Thank You PHP Architecture

---
+++ themes/pns-molecule/framework/views/partials/page/contact-thank-you/_thank-you-col__contact.php
@@ -0,0 +1,49 @@
+<div class="c-title-group c-title-group--contact">
+
+	<h2 class="c-title-group__title e-h6 u-color--blue u-text-up">
+		<?php esc_html_e( 'Address' ); ?>
+	</h2>
+
+	<p class="c-title-group__sub e-p--common">
+		<a class="c-text-link u-color--gray" href="https://www.google.com/maps/place/Pets+In+Stitches/@39.6306803,-84.2302138,17z/data=!3m1!4b1!4m5!3m4!1s0x884089007e771bbd:0x70b4d22ce61cd225!8m2!3d39.6306803!4d-84.2280251" target="_blank">
+			<?php esc_html_e( '8265 Springboro Pike'); ?>
+			<br/>
+			<?php esc_html_e( 'Miamisburg, Ohio 45343'); ?>
+        </a>
+	</p>
+
+</div>
+
+<div class="c-title-group c-title-group--contact">
+
+	<p class="c-title-group__title e-p--common">
+
+		<strong class="u-color--blue"><?php esc_html_e( 'Phone:'); ?></strong> <a class="c-text-link u-color--gray" href="tel:937-630-3320"><?php esc_html_e( '937-630-3320'); ?></a>
+		<br/>
+		<strong class="u-color--blue"><?php esc_html_e( 'Email:'); ?></strong> <a class="c-text-link u-color--gray" href="mailto:info@petsinstitches.com"><?php esc_html_e( 'info@petsinstitches.com'); ?></a>
+		
+	</h2>
+
+
+</div>
+
+
+<div class="c-title-group c-title-group--contact">
+
+	<h2 class="c-title-group__title e-h6 u-color--blue u-text-up">
+		<?php esc_html_e( 'Hours' ); ?>
+	</h2>
+
+	<p class="c-title-group__sub e-p--common">
+		<strong><?php esc_html_e( 'By Appointment Only'); ?></strong>
+        <br/>
+        <?php esc_html_e( 'Open at 7:30am (M - Th)'); ?>
+        <br/>
+        <br/>
+        <strong><?php esc_html_e( 'Wellness Walk-In'); ?></strong>
+        <br/>
+        <?php esc_html_e( '3:00pm - 4:00pm (Tu &amp; W)'); ?>
+        
+	</p>
+
+</div>
---
+++ themes/pns-molecule/framework/views/templates/page/page-contact-thank-you.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template • Contact Thank You
+ *
+ * The template file to render all parts of the 
+ * Contact Thank You Page
+ */
+
+// Intro Section after hero image
+Molecule_Router::render( 'page/contact-thank-you', '_thank-you', 'intro');
+
+
+// Intro Section after hero image
+Molecule_Router::render( 'page/contact-thank-you', '_thank-you', 'next-steps');
