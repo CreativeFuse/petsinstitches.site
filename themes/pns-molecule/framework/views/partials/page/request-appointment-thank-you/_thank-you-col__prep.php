f00fa11a1eb75ac0c07311fdce91dd2106bcde6d
codingwithchris <chris@codewolfseven.com>
2018-04-08 09:44:57 -0400

    Build Appointment Landing PHP Architecture

---
+++ themes/pns-molecule/framework/views/partials/page/request-appointment-thank-you/_thank-you-col__prep.php
@@ -0,0 +1,29 @@
+<div class="c-title-group">
+
+	<h2 class="e-h2 c-title-group__title u-color--blue">
+		<?php esc_html_e( 'Prep for Your Visit'); ?>
+	</h2>
+
+	<p class="e-p--large u-max-width--800 u-center">
+		<?php esc_html_e( 'Consider gathering the following with you before visiting us at our clinic.' ); ?>
+	</p>
+
+	<ul class="c-list-paws c-list-paws--blue">	
+
+		<li class="c-list-paws__item e-p--common"><?php esc_html_e( 'Most recent rabies vaccine is required if you\'re not purchasing rabies vaccines.'); ?></li>
+
+		<li class="c-list-paws__item e-p--common"><?php esc_html_e( 'If you request the booster vaccines, a copy of recent vaccination records is required.'); ?></li>
+
+		<li class="c-list-paws__item e-p--common"><?php esc_html_e( 'Completed and signed admissions and consent forms.'); ?></li>
+
+	</ul>
+
+	<a class="c-btn c-btn--fill c-btn--purple c-btn--hover-outline c-title-group__btn" href="/forms/">
+
+		<div class="c-btn__text">
+			<?php esc_html_e( 'Download Forms'); ?>
+		</div>
+		
+	</a>
+
+</div>
