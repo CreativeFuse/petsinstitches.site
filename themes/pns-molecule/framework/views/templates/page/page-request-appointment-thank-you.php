30d230c152f11b0d1909c4022a3d50fc04685d39
codingwithchris <chris@codewolfseven.com>
2018-04-08 09:51:29 -0400

    Squashed commit of the following:
    commit 2e9cac9d8b032ddbc444c96cea2d80928a9acda0
    Merge: fd5d1ef 6901dda
    Author: Christopher Hahn <codingwithchris@users.noreply.github.com>
    Date:   Sun Apr 8 09:50:47 2018 -0400
    
        Merge pull request #13 from CreativeFuse/8_appointment-landing
    
        #8 appointment landing - correct branch
    
    commit 6901dda5fcf2cffe467d13c4f51989a7ad97a086
    Author: codingwithchris <chris@codewolfseven.com>
    Date:   Sun Apr 8 09:45:37 2018 -0400
    
        Style Appointment Landing Page
    
    commit af31e9e137e4b2579ffb0f0729ea429f854409d7
    Author: codingwithchris <chris@codewolfseven.com>
    Date:   Sun Apr 8 09:45:15 2018 -0400
    
        Add Appointment Thank You Landing Page Template
    
    commit f00fa11a1eb75ac0c07311fdce91dd2106bcde6d
    Author: codingwithchris <chris@codewolfseven.com>
    Date:   Sun Apr 8 09:44:57 2018 -0400
    
        Build Appointment Landing PHP Architecture
    
    commit 5923f735dc288df5cec4e2bec6da80c8459bca3b
    Author: codingwithchris <chris@codewolfseven.com>
    Date:   Sun Apr 8 09:44:40 2018 -0400
    
        Add Margin Above Intro Buttons
    
    commit 0007c03adc3628f668bb9142c85491497d6cb1b2
    Author: codingwithchris <chris@codewolfseven.com>
    Date:   Sun Apr 8 09:44:21 2018 -0400
    
        Add a white button theme
    
    commit 4bbb7148ec5b15d50e3da521173202cb1ee34146
    Author: codingwithchris <chris@codewolfseven.com>
    Date:   Sun Apr 8 09:44:13 2018 -0400
    
        Remove Whitespace
    
    commit b2449863254f85018e69e5dfdc343f6c47a9cbdb
    Author: codingwithchris <chris@codewolfseven.com>
    Date:   Sun Apr 8 09:44:03 2018 -0400
    
        Add white to our color map
    
        Add white to color map to tie in with new appointment “take a tour” button
    
    commit e8175c2362d002e233d7fe513240543034d725ec
    Author: codingwithchris <chris@codewolfseven.com>
    Date:   Sun Apr 8 09:43:34 2018 -0400
    
        Init and re-compile bower

---
+++ themes/pns-molecule/framework/views/templates/page/page-request-appointment-thank-you.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template • Appointment Request Thank You
+ *
+ * The template file to render all parts of the 
+ * Request an Appointment Thank You Page
+ */
+
+// Intro Section after hero image
+Molecule_Router::render( 'page/request-appointment-thank-you', '_thank-you', 'intro');
+
+
+// Intro Section after hero image
+Molecule_Router::render( 'page/request-appointment-thank-you', '_thank-you', 'next-steps');
