6a65f01e465aef503a1f3d3ef25f8bb98894ba1f
Christopher Hahn <codingwithchris@users.noreply.github.com>
2018-12-18 09:47:00 -0500

    Add Animal BG Images to Appointment Request Page (#36)

--- themes/pns-molecule/framework/views/partials/page/request-appointment/_request-appointment-form.php
+++ themes/pns-molecule/framework/views/partials/page/request-appointment/_request-appointment-form.php
@@ -1,3 +1,10 @@
+<?php
+
+	$left_img = get_field( 'request_appointment_bg_left' );
+	$right_img = get_field( 'request_appointment_bg_right' );
+
+?>
+
 <div class="o-section o-section--form">
 	<div class="o-container o-container--max">
 
@@ -13,4 +20,10 @@
 			</div>
 		</div>
 	</div>
+
+	<div class="o-section o-section--request-apt-images">
+		<img class="o-section--request-apt__image o-section--request-apt__image--left" src="<?= esc_url( $left_img['url'] ); ?>" alt="<?= esc_html( $left_img['url'] ); ?>" width="350" height="350">
+		<img class="o-section--request-apt__image o-section--request-apt__image--right" src="<?= esc_url( $right_img['url'] ); ?>" alt="<?= esc_html( $right_img['url'] ); ?>" width="350" height="350">
+	</div>
+
 </div>
