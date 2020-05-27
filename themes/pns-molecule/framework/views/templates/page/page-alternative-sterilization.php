7a87dabdefcc8c92238e45159e6f6df5882e25ae
Nowell VanHoesen <nowell@nvwebdev.com>
2020-05-27 07:17:56 -0400

    missed the view template

--- themes/pns-molecule/framework/views/templates/page/page-alternative-sterilization.php
+++ themes/pns-molecule/framework/views/templates/page/page-alternative-sterilization.php
@@ -3,15 +3,21 @@
 /**
  * Template • Alternative Sterilization
  *
- * The template file to render all parts of the 
+ * The template file to render all parts of the
  * Getting Alternative Sterilization
  */
 
 // Intro Section after hero image
 Molecule_Router::render( 'page/alt-sterilization', '_alt-sterilization', 'intro');
 
+// Low Price Guarantee
+Molecule_Router::render( 'page/alt-sterilization', '_alt-sterilization', 'low-price');
+
 // Main Section, list of different surgeries
 Molecule_Router::render( 'page/alt-sterilization', '_alt-sterilization', 'section');
+
+// Main Section, list of different surgeries
+Molecule_Router::render( 'page/alt-sterilization', '_alt-sterilization', 'comparison');
 
 // Request an Appointment
 Molecule_Router::render( 'object/cta', '_request', 'appointment');
