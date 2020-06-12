a4b0d6686b7f86caa87ca9bb9aabf2f5009531d9
Andy Baker <andy@creativefuse.org>
2020-06-12 09:37:16 -0400

    Update 404 Page Section

--- themes/pns-molecule/framework/views/partials/page/404/_404-section.php
+++ themes/pns-molecule/framework/views/partials/page/404/_404-section.php
@@ -9,13 +9,13 @@
 				<div class="c-title-group c-title-group--404">
 
 					<h2 class="c-title-group__title e-h1 u-color--blue">404</h2>
-					<p class="c-title-group--404__sub e-h4">Similar to pets, this web page need fixed.</p>
+					<p class="c-title-group--404__sub e-h4">Similar to pets, this web page needs fixed.</p>
 					<p class="c-title-group--404__sub e-h4">Let's<a href="<?php echo esc_url('/getting-started/'); ?>" class="u-color--blue u-text-underline"> get started!</a></p>
 
 				</div>
 
 			</div>
-			
+
 			<div class="o-col-md-6">
 
 				<img src="<?php echo esc_url( home_url() ) . '/wp-content/uploads/2017/08/pets-in-stitches-404-hero.jpg'; ?>" />
